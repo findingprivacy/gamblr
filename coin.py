f2711ad3af26d37c6680a13c6ff58e0d1d0ca8df
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-06-24 14:34:26 -0500

    Update coin.py

--- coin.py
+++ coin.py
@@ -3,11 +3,11 @@
 import pyautogui
 from time import sleep
 
-tail = 345, 465
-head = 175, 465
-amount_location = 170, 425
-double = 435, 425
-bet_cashout = 285, 500
+tail = 350, 480
+head = 200, 480
+amount_location = 180, 440
+double = 430, 442
+bet_cashout = 295, 515
 bet_amount = '0.025'
 
 
