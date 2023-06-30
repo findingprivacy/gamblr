d0bdc695c611cd8aef7ba291488032a6eaba0748
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-06-30 18:51:56 -0500

    Update 99out.py

--- 99out.py
+++ 99out.py
@@ -5,9 +5,9 @@ from time import sleep
 from os import system
 
 
-payout_amount = '1.5' # float(input('Payout Amount : '))
+payout_amount = '2' # float(input('Payout Amount : '))
 bet_amount = '0.01' # float(input('Bet Amount : '))
-loss_increase_percentage = '250' # int(input('Percentage Increase on Loss : '))
+loss_increase_percentage = '110' # int(input('Percentage Increase on Loss : '))
 seed_location = 456, 389       
 new_seed = 277, 465
 classic_dice_payout_location = 336, 313
@@ -22,12 +22,12 @@ classic_dice_exit_location = 481, 44
 
 def classicDice():
     #bcgame classic-dice
-    sleep(0.1)
+    sleep(0.25)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
-    sleep(1)
-    pyautogui.write('opera bc.game/game/classic-dice')
-    sleep(1)
+    sleep(.25)
+    pyautogui.write('opera bc.game/game/classic-dice --new-window')
+    sleep(0.25)
     pyautogui.press('enter')
     sleep(10)
     pyautogui.click(seed_location) # change seed
@@ -35,21 +35,21 @@ def classicDice():
     pyautogui.click(new_seed)
     sleep(1)
     pyautogui.click(classic_dice_auto_bet_location)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.doubleClick(classic_dice_bet_amount_location, interval=0.15)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.write(bet_amount) 
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.click(classic_dice_enable_loss_increase_location) 
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.doubleClick(classic_dice_loss_increase_location, interval=0.15)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.write(loss_increase_percentage)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.doubleClick(classic_dice_payout_location, interval=0.1)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.write(payout_amount)
-    sleep(0.1)
+    sleep(0.25)
     pyautogui.click(classic_dice_start_location) # on auto
     sleep(5)
     pyautogui.click(classic_dice_exit_location)
