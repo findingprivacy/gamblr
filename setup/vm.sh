84cf3fef27c3dca614b63add88b03781838b819f
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-07-01 15:59:26 -0500

    Create vm.sh

---
+++ setup/vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y &&
+sudo apt install dwm suckless-tools surf -y &&
+sudo apt install python3-pip -y &&
+sudo apt install python3-tk -y &&
+sudo apt install python3-dev -y &&
+sudo apt install python-is-python3 -y &&
+sudo apt install scrot -y &&
+sudo apt install chromium-browser -y &&
+python -m pip install pyautogui &&
+sudo apt upgrade -y &&
