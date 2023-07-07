b89aeaa18eb21c35bd8158f093ceec947f03aa63
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-07-07 14:23:24 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,50 +1,27 @@
 #!/bin/python3
+#1024x768
 import pyautogui
 from time import sleep
 tail = 174, 250
 head = 79, 250
-amount_location = 93, 209
-double = 177, 212
-bet_cashout = 133, 290
-bet_amount = '0.00001'
-bankroll = '0.87381'
-address_bar = 175, 73
-vault_url = 'https://bc.game/'
-vault_tab = 321, 37
-vault_profile = 441, 112
-vault_wallet = 377, 147
-vault_pro = 97, 307
-vault_max = 327, 256
-vault_transfer = 289, 293
-vault_in = 215, 180
-vault_out = 310, 180
-vault_amount = 204, 254
-coin_url = 'https://bc.game/game/coinflip'
-coin_tab = 50, 38
+amount_location = 159, 225
+double = 235, 225
+bet_cashout = 195, 300
+bet_amount = '0.001'
+bankroll = '0.341'
+vault_tab = 250, 45
+vault_max = 384, 267
+vault_transfer = 345, 310
+vault_in = 275, 193
+vault_out = 375, 193
+vault_amount = 275, 270
+coin_tab = 118, 45
 def heads():
     sleep(0.1)
-    win = pyautogui.pixelMatchesColor(425, 306, (59, 193, 23))
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
     if win:
         pyautogui.click(bet_cashout)
         sleep(1)
-        pyautogui.click(vault_tab)
-        sleep(0.1)
-        pyautogui.click(vault_in)
-        sleep(0.1)
-        pyautogui.click(vault_max)
-        sleep(0.1)
-        pyautogui.click(vault_transfer)
-        sleep(0.1)
-        pyautogui.click(vault_out)
-        sleep(0.1)
-        pyautogui.doubleClick(vault_amount, interval=0.25)
-        sleep(0.1)
-        pyautogui.write(bankroll)
-        sleep(0.1)
-        pyautogui.click(vault_transfer)
-        sleep(0.1)
-        pyautogui.click(coin_tab)
-        sleep(0.1)
         pyautogui.doubleClick(amount_location, interval=0.25)
         sleep(0.25)
         pyautogui.write(bet_amount)
@@ -57,44 +34,8 @@ def heads():
     sleep(1)
     pyautogui.click(head)
     sleep(1)
-def tails():
-    sleep(0.1)
-    win = pyautogui.pixelMatchesColor(425, 306, (59, 193, 23))
-    if win:
-        pyautogui.click(bet_cashout)
-        sleep(1)
-        pyautogui.click(vault_tab)
-        sleep(0.1)
-        pyautogui.click(vault_in)
-        sleep(0.1)
-        pyautogui.click(vault_max)
-        sleep(0.1)
-        pyautogui.click(vault_transfer)
-        sleep(0.1)
-        pyautogui.click(vault_out)
-        sleep(0.1)
-        pyautogui.doubleClick(vault_amount, interval=0.25)
-        sleep(0.1)
-        pyautogui.write(bankroll)
-        sleep(0.1)
-        pyautogui.click(vault_transfer)
-        sleep(0.1)
-        pyautogui.click(coin_tab)
-        sleep(0.1)
-        pyautogui.doubleClick(amount_location, interval=0.25)
-        sleep(0.25)
-        pyautogui.write(bet_amount)
-    else:
-        pyautogui.click(double)
-        sleep(0.1)
-        pyautogui.click(double)
-        sleep(0.1)
-    pyautogui.click(bet_cashout)
-    sleep(1)
-    pyautogui.click(tail)
-    sleep(1)
 def vault():
-    win = pyautogui.pixelMatchesColor(425, 306, (59, 193, 23))
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
     if win:
         pyautogui.click(bet_cashout)
         sleep(1)
@@ -119,6 +60,11 @@ def vault():
         pyautogui.doubleClick(amount_location, interval=0.25)
         sleep(0.25)
         pyautogui.write(bet_amount)
+        sleep(0.1)
+        pyautogui.click(bet_cashout)
+        sleep(1)
+        pyautogui.click(head)
+        sleep(1)
     else:
         sleep(0.1)
         pyautogui.click(vault_tab)
@@ -142,8 +88,21 @@ def vault():
         pyautogui.doubleClick(amount_location, interval=0.25)
         sleep(0.25)
         pyautogui.write(bet_amount)
+        sleep(0.1)
+        pyautogui.click(bet_cashout)
+        sleep(1)
+        pyautogui.click(head)
+        sleep(1)
 def main():
-    win = pyautogui.pixelMatchesColor(425, 306, (59, 193, 23))
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
+    vault()
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
+    heads()
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
+    heads()
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
+    heads()
+    win = pyautogui.pixelMatchesColor(484, 321, (42, 45, 51))
     heads()
 while True:
     main()
