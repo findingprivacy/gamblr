7f4e2176e99a080a73d7d3c47d13d2ad52220039
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-07-07 19:42:59 -0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -7,7 +7,7 @@ limbo_start_location = 193, 518
 ultimate_dice_tab = 236, 42
 ultimate_dice_start_location = 195, 524
 number_of_bets = 130, 279
-bets = '10'
+bets = '5'
 def classicDice():
     pyautogui.click(classic_dice_tab)
     sleep(0.25)
