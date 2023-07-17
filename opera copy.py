3d6a96f6f10708d52d6ad185c0135d0a8e7cdbcb
findingprivacy <101117364+findingprivacy@users.noreply.github.com>
2023-07-17 17:32:08 -0500

    idk

--- opera copy.py
+++ opera copy.py
@@ -1,7 +1,5 @@
 import pyautogui
 from time import sleep
-##### bet info
-
 numbet = '99'
 bet_amount = '0.001'
 double = 232, 237
@@ -11,7 +9,7 @@ classic_dice_new_seed = 302, 602
 classic_dice_tab = 100, 40
 classic_dice_bet_amount_location = 153, 236
 classic_dice_start_location = 191, 526
-def classicDice():
+def classicDiceStarter():
     win = pyautogui.pixelMatchesColor(495, 195, (59, 193, 23))
     sleep(0.25)
     if win:
@@ -19,17 +17,26 @@ def classicDice():
         sleep(2.5)
         pyautogui.click(classic_dice_new_seed)
         sleep(2)
-        pyautogui.doubleClick(classic_dice_bet_amount_location, interval=0.15)
+    else:
         sleep(0.1)
-        pyautogui.write(bet_amount) 
-        sleep(0.1)
-        pyautogui.doubleClick(numbetloc, interval=0.15)
-        sleep(0.1)
-        pyautogui.write(numbet) 
-        sleep(0.1)
-        pyautogui.click(classic_dice_start_location)
+    pyautogui.doubleClick(classic_dice_bet_amount_location, interval=0.15)
+    sleep(0.1)
+    pyautogui.write(bet_amount) 
+    sleep(0.1)
+    pyautogui.doubleClick(numbetloc, interval=0.15)
+    sleep(0.1)
+    pyautogui.write(numbet) 
+    sleep(0.1)
+    pyautogui.click(classic_dice_start_location)
+    sleep(75)
+def classicDiceHelper():
+    win = pyautogui.pixelMatchesColor(495, 195, (59, 193, 23))
+    sleep(0.25)
+    if win:
         sleep(0.1)
     else:
+        pyautogui.click(double) 
+        sleep(0.1)
         pyautogui.click(double) 
         sleep(0.1)
         pyautogui.doubleClick(numbetloc, interval=0.15)
@@ -37,9 +44,9 @@ def classicDice():
         pyautogui.write(numbet) 
         sleep(0.1)
         pyautogui.click(classic_dice_start_location)
-        sleep(0.1)
+        sleep(75)
 def main():
-    classicDice()
-    sleep(75)
+    classicDiceStarter()
+    classicDiceHelper()
 while True:
     main()
